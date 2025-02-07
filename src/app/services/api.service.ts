429fd7e5081b9122cf56dd598a5a1961c5406157
Osvaldo <osvaldovdc12@gmail.com>
2025-02-07 14:02:54 -0300

    Correção de código

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,7 +7,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = environment.apiUrl
+  private apiUrl = environment.API_URL
   
   constructor(private http: HttpClient) {}
   
